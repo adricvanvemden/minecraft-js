a4fa68c93631229287080d0986c0d75ad09146c0
Sarah Mcmahon <adricvanvemden@gmail.com>
2024-03-10 18:17:39 +0100

    add simplexNoise for terrain generation

--- scripts/world.js
+++ scripts/world.js
@@ -1,16 +1,92 @@
 import * as THREE from 'three';
+import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js';
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00ddae });
 
 export class World extends THREE.Group {
-    constructor(size = {width: 64, height: 32}) {
+    /**
+     * @type {{
+     *  id: number
+     *  instanceId: number
+     * }[][][]}
+    */
+    data = [];
+
+    params = {
+        terrain: {
+            scale: 30,
+            magnitude: 0.5,
+            offset: 0.2
+        }
+    };
+
+
+
+    constructor(size = { width: 64, height: 32 }) {
         super();
         this.size = size;
     }
 
     generate() {
+        this.initialiseTerrain();
+        this.generateTerrain();
+        this.generateMeshes();
+    }
+
+    /**
+     * Initialising the world terrain data
+    */
+    initialiseTerrain() {
+        this.data = [];
+        for (let x = 0; x < this.size.width; x++) {
+            const slice = []
+            for (let y = 0; y < this.size.height; y++) {
+                const row = [];
+                for (let z = 0; z < this.size.width; z++) {
+                    row.push({ id: 0, instanceId: null });
+                }
+                slice.push(row);
+            }
+            this.data.push(slice);
+        }
+    }
+
+
+    /**
+     * Generates the world terrain data
+    */
+    generateTerrain() {
+        const simplexNoise = new SimplexNoise();
+        for (let x = 0; x < this.size.width; x++) {
+            for (let z = 0; z < this.size.width; z++) {
+
+                // Compute the noise value for the current (x, z) coordinates
+                const value = simplexNoise.noise(x / this.params.terrain.scale, z / this.params.terrain.scale);
+
+                // Scale the noise based on the magnitude and offset
+                const scaledNoise =  this.params.terrain.offset + this.params.terrain.magnitude * value;
+
+                // Compute the height of the terrain at (x, z) based on the scaled noise
+                let height = Math.floor(this.size.height * scaledNoise);
+
+                // Clamp the height to the world's height
+                height = Math.max(0, Math.min(height, this.size.height - 1));
+
+                // Fill the terrain up to the height
+                for(let y = 0; y <= height; y++){
+                    this.setBlockId(x, y, z, 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Generates the 3D representation of the world
+    */
+    generateMeshes() {
         this.clear();
+
         const maxCount = this.size.width * this.size.height * this.size.width;
         const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
         mesh.count = 0;
@@ -19,11 +95,77 @@ export class World extends THREE.Group {
         for (let x = 0; x < this.size.width; x++) {
             for (let y = 0; y < this.size.height; y++) {
                 for (let z = 0; z < this.size.width; z++) {
-                    matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
-                    mesh.setMatrixAt(mesh.count++, matrix);
+                    const blockId = this.getBlock(x, y, z).id;
+                    const instanceId = mesh.count;
+
+                    if (blockId !== 0) {
+                        matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+                        mesh.setMatrixAt(instanceId, matrix);
+                        this.setBlockInstanceId(x, y, z, instanceId);
+                        mesh.count++;
+                    }
                 }
             }
         }
         this.add(mesh)
+    }
+
+    /**
+      * Gets the block data at (x, y, z)
+      * @param {number} x 
+      * @param {number} y 
+      * @param {number} z 
+      * @returns {{id: number, instanceId: number}}
+      */
+    getBlock(x, y, z) {
+        if (this.inBounds(x, y, z)) {
+            return this.data[x][y][z];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the block id for the block at (x, y, z)
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     * @param {number} id
+    */
+    setBlockId(x, y, z, id) {
+        if (this.inBounds(x, y, z)) {
+            this.data[x][y][z].id = id;
+        }
+    }
+
+
+    /**
+     * Sets the block instance id for the block at (x, y, z)
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     * @param {number} instanceId
+    */
+    setBlockInstanceId(x, y, z, instanceId) {
+        if (this.inBounds(x, y, z)) {
+            this.data[x][y][z].instanceId = instanceId;
+        }
+    }
+
+    /**
+     * Checks if the (x, y, z) coordinates are within bounds
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} z 
+     * @returns {boolean}
+    */
+    inBounds(x, y, z) {
+        if (x >= 0 && x < this.size.width &&
+            y >= 0 && y < this.size.height &&
+            z >= 0 && z < this.size.width) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
